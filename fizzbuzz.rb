ed9b5c743d6eba06e6432acb0002a9d32ff7365e
Carrie Mathieu <carrieburks@gmail.com>
2020-08-06 02:03:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,11 +4,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  if int % 5 == 0 
+  elsif int % 5 == 0 
     "Buzz"
   if int % 5 == 0 && int % 3 == 0 
     "FizzBuzz"
-  end
-  end
   end
 end
